3ada730de7b553c998d5073adbc14691704a2355
Samahir Bantan <52120479+samahirban@users.noreply.github.com>
2020-03-10 07:36:11 +0300

    Create algorithm.js

---
+++ algorithm.js
@@ -0,0 +1,9 @@
+//Graph class to represent visualization map
+function Graph(){
+  this.vert = [];
+  this.vert_num = 0;
+  this.edge_num = 0;
+  this.weighted = false;
+  this.connected_num = 0;
+  this.label = "";
+}
