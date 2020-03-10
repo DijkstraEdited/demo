617dc718474007336877623828c827f1de261fd4
Samahir Bantan <52120479+samahirban@users.noreply.github.com>
2020-03-10 08:19:26 +0300

    Update algorithm.js

--- algorithm.js
+++ algorithm.js
@@ -3,7 +3,30 @@ function Graph(){
   this.vert = [];
   this.vert_num = 0;
   this.edge_num = 0;
-  this.weighted = false;
   this.connected_num = 0;
   this.label = "";
+}
+//Vertex class to represent package location
+function Vertex(v){
+  this.label = v.label;//<--Could be coordinates of package location
+  this.visit = false;
+  this.adjacent = new List();
+}
+//Edge class to represent path of location
+function Edge(t, d){
+  this.target = t;
+  this.distanc = d;
+}
+//Create map
+function create_map(vert, edge){
+  this.vertnum = vert.length;
+  this.edgenum = vert.length;
+  //Add vertex into the array
+  for(var i = 0; i < this.vertnum; i++)
+    this.vertex[i] = new Vertex(vert[i]);
+  //Add edge to vertex
+  for(var i = 0; i < this.edgenum; i++){
+    var edge = new Edge(edge[i]);
+    this.adjacent.insert(edge);
+  }
 }
