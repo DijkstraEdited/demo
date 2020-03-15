9223a0b7ae0666dc7ee7f333aa4435e6ec2134fb
Afrah Khader <afrah.education@gmail.com>
2020-03-15 22:16:08 +0300

    genetic algorithm

---
+++ Genetic.js
@@ -0,0 +1,68 @@
+
+function main() {
+    var _v = [
+        { label: "a" }, // index = 0
+        { label: "b" }, // index = 1
+        { label: "c" }, // index = 2
+        { label: "d" }, // index = 3
+        { label: "e" }, // index = 4
+    ];
+
+    var _e = [
+        { u: 0, t: 1, d: 5 },
+        { u: 0, t: 2, d: 11 },
+        { u: 0, t: 3, d: 8 },
+        { u: 0, t: 4, d: 9 },
+
+        { u: 1, t: 0, d: 12 },
+        { u: 1, t: 2, d: 11 },
+        { u: 1, t: 3, d: 5 },
+        { u: 1, t: 4, d: 10 },
+
+        { u: 2, t: 0, d: 6 },
+        { u: 2, t: 1, d: 16 },
+        { u: 2, t: 3, d: 13 },
+        { u: 2, t: 4, d: 5 },
+
+        { u: 3, t: 0, d: 20 },
+        { u: 3, t: 1, d: 8 },
+        { u: 3, t: 2, d: 30 },
+        { u: 3, t: 4, d: 7 },
+
+        { u: 4, t: 0, d: 12 },
+        { u: 4, t: 1, d: 15 },
+        { u: 4, t: 2, d: 10 },
+        { u: 4, t: 3, d: 3 },
+
+
+    ];
+}
+function fitness() {
+
+}
+
+function population() {
+
+}
+
+function generatChromosome() {
+
+}
+
+function generatGeneration() {
+
+}
+
+function mutation() {
+
+}
+
+
+function crossOver() {
+
+}
+
+
+
+
+
