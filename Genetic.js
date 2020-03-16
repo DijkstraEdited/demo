42af3be0607fd6855900a7213c03e329d40bcbe6
Samahir Bantan <52120479+samahirban@users.noreply.github.com>
2020-03-16 16:45:52 +0300

    Update Genetic.js

--- Genetic.js
+++ Genetic.js
@@ -1,4 +1,4 @@
-
+//Main of the javascript
 function main() {
     var _v = [
         { label: "a" }, // index = 0
@@ -33,10 +33,102 @@ function main() {
         { u: 4, t: 1, d: 15 },
         { u: 4, t: 2, d: 10 },
         { u: 4, t: 3, d: 3 },
-
-
     ];
+    //Create graph
+    var g = new Graph();
+    g.create_map(_v, _e);
+    document.write("<br>Graph: <br>");
+    g.printGraph();
+    
+    //Apply genetic algorithm
+    
 }
+
+//Graph class to represent visualization map
+function Graph() {
+    this.vert = [];
+    this.vertnum = 0;
+    this.edgenum = 0;
+    this.label = "";
+    //methods of graph
+    this.create_map = createMapFunction;
+    this.printGraph = printGraphFunction;
+}
+
+//Vertex class to represent package location
+function Vertex(v) {
+    this.label = v.label;//<--Could be coordinates of package location
+    this.visit = false;
+    this.adjacent = new List();
+    //Functions of vertex
+    this.insertAdj = insertAdjacent;
+    this.adjacentById = adjacentByIdImpl;
+    this.distanceById = distancByIdFunction;
+    this.vertexInfo = vertexInfoImpl;
+}
+
+//Edge class to represent path of location
+function Edge(t, d) {
+    this.target = t;
+    this.distanc = d;
+}
+
+//Create map
+function createMapFunction(v, e) {
+    this.vertnum = v.length;
+    this.edgenum = e.length;
+    //Add vertex into the array
+    for (var i = 0; i < this.vertnum; i++) {
+        this.vert[i] = new Vertex(v[i]);
+    }
+    //Add edge to vertex
+    for (var i = 0; i < this.edgenum; i++) {
+        var u = this.vert[e[i].u];
+        var v = this.vert[e[i].t];
+        u.insertAdj(e[i].t, e[i].d);
+    }
+
+}
+
+//Insert adjacente(s)
+function insertAdjacent(t, d) {
+    var edge = !(d === undefined) ? new Edge(t, d) : new Edge(t);
+    this.adjacent.insert(edge);
+}
+
+//Print method
+function printGraphFunction() {
+    for (var i = 0; i < this.vertnum; i++) {
+        var v = this.vert[i];
+        document.write("VERTEX: ", i, v.vertexInfo(), "<br>");
+    }
+}
+
+//Cont. print
+function vertexInfoImpl() {
+    return (" {" + this.label + "} - VISIT: " + this.visit + " - ADJACENCY: " + this.adjacentById() + " - DISTANC: " + this.distanceById());
+}
+
+//Get adjacents nodes
+function adjacentByIdImpl() {
+    var adjacent_Id = [];
+    var edge_adj = this.adjacent.traverse();
+    for (var i = 0; i < edge_adj.length; i++) {
+        adjacent_Id[i] = edge_adj[i].target;
+    }
+    return adjacent_Id;
+}
+
+//Get distanc between two nodes
+function distancByIdFunction() {
+    var distances = [];
+    var edge_adj = this.adjacent.traverse();
+    for (var i = 0; i < edge_adj.length; i++) {
+        distances[i] = edge_adj[i].distanc;
+    }
+    return distances;
+}
+
 
 function fitness() {
     for (var i = 0 ; i < population.length ; i++){
