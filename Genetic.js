6c2f668fabf850cbf74d2929b391365808db9904
ShorooqMahdi <61960507+ShorooqMahdi@users.noreply.github.com>
2020-03-16 19:00:13 +0300

    Update Genetic.js

--- Genetic.js
+++ Genetic.js
@@ -229,3 +229,20 @@ function shuffle(array) {
     }
     return array;
 }
+
+function calcDistance(points, order){
+    var sum = 0;
+
+    for (var i = 0; i < order.length ; i++){
+        var cityAIndex = order[i];
+        var cityA = points[cityAIndex];
+        var cityBIndex = order[i + 1];
+        var cityB = points[cityBIndex];
+        var d = dist(cityA.x, cityA.y, cityB.x, cityB.y);
+        sum += d;
+    }
+
+    return sum;
+}
+
+
